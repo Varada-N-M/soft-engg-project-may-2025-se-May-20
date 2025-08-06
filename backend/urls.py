c3f56e371cfb585096465da0598a3ec8936e9687
23f2004169 <23f2004169@ds.study.iitm.ac.in>
2025-08-06 15:28:41 +0530

    Add urls to main branch

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,9 @@
 from flask_restful import Api
 from api.auth.routes import SignupChild, Login, RefreshToken, SignupParent, SignupOrganization, SignupTeacher
-from api.student.routes import GratitudeEntry, Habits, CompleteHabit, ToDoListResource
+from api.student.routes import GratitudeEntry, Habits, CompleteHabit, ToDoListResource, StudentLessonUpdates
 from api.parent.routes import LinkChildToParent
 from api.student.routes import SkillResource, CompleteSkillResource, SkillStatsResource, SkillSearchResource
-from api.teacher.routes import AddStudent, RemoveStudent
+from api.teacher.routes import AddStudent, RemoveStudent, TeacherLessonUpdates
 from api.organization.routes import OrganizationStats
 from api.auth.routes import ForgotPassword, ResetPassword, ChangePassword
 
@@ -26,6 +26,7 @@ api.add_resource(GratitudeEntry, '/api/child/gratitude', '/api/child/gratitude/<
 api.add_resource(Habits, '/api/child/habit', '/api/child/habit/<int:habit_id>')
 api.add_resource(CompleteHabit, '/api/child/habit/<int:habit_id>/complete')
 api.add_resource(ToDoListResource, '/api/todos', '/api/todos/<int:todo_id>')
+api.add_resource(StudentLessonUpdates, '/api/child/lesson-updates')
 
 # Parent
 api.add_resource(LinkChildToParent, '/api/parent/link-child', '/api/parent/unlink-child/<int:child_id>')
@@ -39,6 +40,7 @@ api.add_resource(SkillSearchResource, '/skills/search')
 # Teacher
 api.add_resource(AddStudent, '/api/teacher/add-student')
 api.add_resource(RemoveStudent, '/api/teacher/remove-student/<int:student_id>')
+api.add_resource(TeacherLessonUpdates, '/api/teacher/lesson-updates')
 
 # Organization
 api.add_resource(OrganizationStats, '/api/organization/stats')

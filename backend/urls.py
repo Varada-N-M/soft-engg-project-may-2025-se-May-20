e170a180d9ad4254855e3323df8e20cdea72347d
Hari <23f1000774@ds.study.iitm.ac.in>
2025-08-07 10:54:40 +0530

    add: admin-register,add-school and skills urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,9 @@
 from flask_restful import Api
-from api.auth.routes import SignupChild, Login, RefreshToken, SignupParent, SignupOrganization, SignupTeacher
+from api.auth.routes import SignupChild, Login, RefreshToken, SignupParent, SignupOrganization, SignupTeacher, SignupAdmin
 from api.student.routes import GratitudeEntry, Habits, CompleteHabit, ToDoListResource, StudentLessonUpdates
 from api.parent.routes import LinkChildToParent
-from api.student.routes import SkillResource, CompleteSkillResource, SkillStatsResource, SkillSearchResource
-from api.teacher.routes import AddStudent, RemoveStudent, TeacherLessonUpdates, TeacherLessonUpdateDetail
+from api.student.routes import Skills
+from api.teacher.routes import AddStudent, RemoveStudent, TeacherLessonUpdates, TeacherLessonUpdateDetail, CreateSchool
 from api.organization.routes import OrganizationStats
 from api.auth.routes import ForgotPassword, ResetPassword, ChangePassword
 
@@ -16,6 +16,9 @@ api.add_resource(SignupChild, '/api/child/register')
 api.add_resource(SignupParent, '/api/parent/register')
 api.add_resource(SignupOrganization, '/api/organization/register')
 api.add_resource(SignupTeacher, '/api/teacher/register')
+api.add_resource(SignupAdmin, '/api/admin/register')
+
+api.add_resource(CreateSchool, '/api/admin/add-school')
 
 api.add_resource(ForgotPassword, '/api/auth/forgot-password')
 api.add_resource(ResetPassword, '/api/auth/reset-password')
@@ -27,16 +30,11 @@ api.add_resource(Habits, '/api/child/habit', '/api/child/habit/<int:habit_id>')
 api.add_resource(CompleteHabit, '/api/child/habit/<int:habit_id>/complete')
 api.add_resource(ToDoListResource, '/api/todos', '/api/todos/<int:todo_id>')
 api.add_resource(StudentLessonUpdates, '/api/child/lesson-updates')
+api.add_resource(Skills, '/api/child/skills', '/api/child/skills/<int:skill_id>')
 
 # Parent
 api.add_resource(LinkChildToParent, '/api/parent/link-child', '/api/parent/unlink-child/<int:child_id>')
 
-
-# Register the resources with their URL patterns
-api.add_resource(SkillResource, '/skills', '/skills/<int:skill_id>')
-api.add_resource(CompleteSkillResource, '/skills/<int:skill_id>/complete')
-api.add_resource(SkillStatsResource, '/skills/stats')
-api.add_resource(SkillSearchResource, '/skills/search')
 # Teacher
 api.add_resource(AddStudent, '/api/teacher/add-student')
 api.add_resource(RemoveStudent, '/api/teacher/remove-student/<int:student_id>')

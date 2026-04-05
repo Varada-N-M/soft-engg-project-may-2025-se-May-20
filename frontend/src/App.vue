1f0048281b97d4d4fa34481197e3c9835b03b598
Developer <developer@example.com>
2026-04-05 07:47:09 +0530

    changes for deployment

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,9 @@
-<!-- <script setup lang="ts">
-
-</script>
-
 <template>
-  <!-- Navbar for authenticated user : todo : update v-if when login completed -->
-  <!-- <auth-navbar v-if="true" :user-type="'student'"/> -->
-  <!-- Navbar for for guest user -->
-  <!-- <guest-navbar v-else /> -->
-  <!-- <router-view></router-view> -->
-<!-- </template> -->
-
-<style scoped>
-
-</style> -->
-<template>
-  <FriendlyCharacter />
+  <router-view></router-view>
 </template>
 
 <script setup>
-import FriendlyCharacter from "./pages/student/FunnyCharacter.vue";
 </script>
+
+<style scoped>
+</style>
